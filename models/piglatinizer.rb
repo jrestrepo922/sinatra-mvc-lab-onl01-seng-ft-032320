b85c013ed6ba9a0ae5cef12d047309ad6928a951
JUAN M RESTREPO <tinto922@gmail.com>
2020-04-30 18:15:19 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -4,8 +4,8 @@ class PigLatinizer
   attr_accessor :piglatinized_phrase
 
   def piglatinize_word(word) #pig latinizes a single word
-    #if it contains a vovel on the first letter of the word enter the if statement and add way to the   end of the input 
-    if word.downcase.index(/[aeiou]/) == 0 
+    #if it contains a vovel on the first letter of the word enter the if statement and add way to the   end of the input
+    if word.downcase.index(/[aeiou]/) == 0
       word + "way"
     else
       vowel_index = word.index(/[aeiou]/)
@@ -13,11 +13,10 @@ class PigLatinizer
       word + front_end +"ay"
     end
   end
-  
+
   def piglatinize(phrase)
     word_array = phrase.split(" ")
     @piglatinized_phrase = word_array.collect {|word| piglatinize(word)}.join(" ")
   end
-  
-end
 
+end
